516de6815c8be74d0d884e0fbaa62918e0545ecd
Kartikx2005 <kartikxdev2005@gmail.com>
2024-09-22 11:35:43 +0530

    2

--- app/components/header.jsx
+++ app/components/header.jsx
@@ -1,7 +1,11 @@
 import Link from 'next/link';
 import { UserButton } from '@clerk/nextjs';
+import { auth, currentUser } from '@clerk/nextjs/server'
+const Header = async ({ username }) => {
+  const { userId } = auth();
+  const user = await currentUser();
+  console.log(user);
 
-const Header = ({ username }) => {
   return (
     <nav className='bg-blue-700 py-4 px-6 flex items-center justify-between mb-5'>
       <div className='flex items-center'>
@@ -11,13 +15,33 @@ const Header = ({ username }) => {
           </div>
         </Link>
       </div>
-      <div className='text-white'>
-        <Link href='sign-in' className='text-gray-300 hover:text-white mr-4'>
-          Sign In
-        </Link>
-        <Link href='sign-up' className='text-gray-300 hover:text-white mr-4'>
-          Sign Up
-        </Link>
+
+      <div className='text-white flex items-center'>
+        {!userId && (
+          <>
+            <Link
+              href='sign-in'
+              className='text-gray-300 hover:text-white mr-4'
+            >
+              Sign In
+            </Link>
+            <Link
+              href='sign-up'
+              className='text-gray-300 hover:text-white mr-4'
+            >
+              Sign Up
+            </Link>
+          </>
+        )}
+
+        {userId && (
+          <>
+            <Link href='profile' className='text-gray-300 hover:text-white mr-4'>
+              Profile
+            </Link>
+          </>
+        )}
+
         <div className='ml-auto'>
           <UserButton afterSignOutUrl='/' />
         </div>
--- app/layout.jsx
+++ app/layout.jsx
@@ -1,8 +1,8 @@
 import './globals.css';
 import { Inter } from 'next/font/google';
 import { ClerkProvider } from '@clerk/nextjs';
-import Header  from './components/header'
-
+import Header from './components/header'
+import { dark } from '@clerk/themes';
 const inter = Inter({ subsets: ['latin'] });
 
 export const metadata = {
@@ -12,7 +12,9 @@ export const metadata = {
 
 export default function RootLayout({ children }) {
   return (
-    <ClerkProvider>
+    <ClerkProvider appearance={{
+      baseTheme: dark,
+    }}>
       <html lang='en'>
         <body className={inter.className}>
           <Header />
